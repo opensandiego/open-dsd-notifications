688cf18fd2ad8884d2ce5e3e45d29fc6e4f48f1f
Dennis Slade <webmaster@sdtf.org>
2015-06-06 14:31:15 -0700

    Added Discretionary Approvals route

--- app.js
+++ app.js
@@ -61,7 +61,51 @@ router.get('/', function(req, res) {
   });
 });
 
+
+// Route for Discretionary Approvals. Access/GET http://localhost:8080/approvals)
+router.get('/approvals', function(req, res) {
+  var dsdURL = "http://opendsd.sandiego.gov/web/Api/Maps/ApprovalsMap?SearchType=Discretionary&SouthWestLatitude=32.71879985593221&SouthWestLongitude=-117.16525563507082&NorthEastLatitude=32.74399836325726&NorthEastLongitude=-117.12534436492922&Page=1&PageLimit=100";
+  
+  request(dsdURL, function(error, response, body) {
+    var body = JSON.parse(body);
+    var features = new Array();
+    _.each(body.results, function(element, index) {
+      // Change fields based on permit type. Need to make flexible for other OpenDSD types, also, need
+      // to understand what the "Case Depiction" field actually does.
+      
+      var titleString = element.ApprovalType+" approval for project \"" +element.ProjectTitle+"\".<br/> You can find out more at: http://opendsd.sandiego.gov/web/Approvals/Details/"+element.ApprovalId
+
+      var feature = {
+        id: element.caseId,
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [element.Longitude, element.Latitude],
+        },
+        properties: {
+          "ApprovalId":element.ApprovalId,
+          "ApprovalType": element.ApprovalType,
+          "ApprovalScope": element.ApprovalScope,
+          "CompleteCancelDate":element.CompleteCancelDate,
+          "IssuedDate":element.IssuedDate,
+          "ProjectId":element.ProjectId,
+          "ProjectTitle":element.ProjectTitle,
+          "title": titleString,
+        }
+      };
+      features.push(feature);
+    });
+    var resp = {
+      "type": "FeatureCollection",
+      "features": features
+    }
+    return res.json(resp);
+  });
+});
+
+
 // more routes for our API will happen here
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
